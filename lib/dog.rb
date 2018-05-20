8208b7afd608899f56c49aca5f23f6075416222b
Ariel Zhen <zhenpingping@live.com>
2018-05-20 06:54:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,7 @@
 class dog
-  
-end 
 
-fido = Dog.new 
-snoopy = Dog.new 
-lassie = Dog.new 
+end
 
-
-
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class person
+
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new 
